74bd394f8eb6b5e0fc60926064d4f5446d1042b2
im23b-busere <busere@bzz.ch>
2025-11-11 15:04:52 +0100

    Create JPA Repositories

---
+++ src/main/java/ch/bzz/repository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package ch.bzz.repository;
+
+import ch.bzz.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, String> {
+}
